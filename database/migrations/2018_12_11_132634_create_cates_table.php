6a07b4e4561ed94d69cc61d8406095c4edc16a60
baba <2338832621@qq.com>
2018-12-11 21:36:35 +0800

    project

--- database/migrations/2018_12_11_132634_create_cates_table.php
+++ database/migrations/2018_12_11_132634_create_cates_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateTextTable extends Migration
+class CreateCatesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,11 +13,11 @@ class CreateTextTable extends Migration
      */
     public function up()
     {
-        Schema::create('text', function (Blueprint $table) {
+        Schema::create('cates', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
-            $table->string('email')->unique();   
-            $table->string('password',60);
+            $table->integer('pid');
+            $table->string('path');
             $table->timestamps();
         });
     }
@@ -29,6 +29,6 @@ class CreateTextTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('text');
+        Schema::dropIfExists('cates');
     }
 }
